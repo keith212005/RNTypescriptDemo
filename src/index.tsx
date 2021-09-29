6970e71437afea213cfe8b497d047b4aa45eefcc
Ketan <keith212005@gmail.com>
2021-09-30 01:00:26 +0530

    second commit

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import StackNavigator from './navigator/stackNavigator';
+
+const AppContainer = () => {
+  return <StackNavigator />;
+};
+
+export default AppContainer;
---
+++ src/navigator/stackNavigator.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const StackNavigator = () => {
+  return (
+    <View style={styles.container}>
+      <Text>StackNavigatorß</Text>
+    </View>
+  );
+};
+
+export default StackNavigator;
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    // backgroundColor: 'white',
+  },
+});
